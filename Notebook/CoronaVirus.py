f575060f2919daf6059f64d1441cfc054c1eef43
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-04-29 23:07:02 -0500

    Updated File

--- Notebook/CoronaVirus.py
+++ Notebook/CoronaVirus.py
@@ -252,8 +252,10 @@ def main():
     corona.plot_state(state='New York',last_30_days=True)
     corona.plot_state(state='Florida',last_30_days=True)
     corona.plot_state(state='Kentucky',last_30_days=True)
+    corona.plot_state(state='Texas',last_30_days=True)
+    corona.plot_state(state='New Jersey',last_30_days=True)
     corona.plot_multi_state(states=['New York','Florida','Illinois','Kentucky'],last_30_days=False)
-    corona.rankState(N=5,daterank='2020-04-27')
+    corona.rankState(N=5,daterank='2020-04-28')
 
 
 main()
